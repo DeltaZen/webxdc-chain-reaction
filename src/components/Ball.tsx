4c5c852e13936bdd0e6753c5b1fa4d94e4daa9e0
Massick <4ndyraul@gmail.com>
2022-10-12 09:50:45 -0400

    style balls as molecules

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -1,18 +1,20 @@
 // import * as React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 
 const Ball = ({ color, clicksToBlow, className }) => {
   const ballStyle = {
-    background: `radial-gradient(circle at 30% 30%, ${color}, black)`,
-  }
+    'background': `radial-gradient(circle at 30% 30%, ${color}, black)`,
+    '--color': color,
+  } as React.CSSProperties
 
   let classes = 'ball'
   if (!className) {
     if (clicksToBlow === 1)
-      classes += ' shaking-more large-ball'
+      classes += ' shaking-more third-ball'
 
     else if (clicksToBlow === 2)
-      classes += ' shaking medium-ball'
+      classes += ' shaking second-ball'
   }
   else {
     classes += ` ${className}`
